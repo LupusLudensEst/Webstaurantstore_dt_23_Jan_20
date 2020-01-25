c10acfed10c586ae77b40f2dc05a8ee16e4ac0da
LupusLudensEst <gurovvic@gmail.com>
2020-01-25 12:14:22 -0500

    upgrade dt 25 Jan 2020

--- features/steps/Webstaurantstore_dt_23_Jan_20.py
+++ features/steps/Webstaurantstore_dt_23_Jan_20.py
@@ -17,7 +17,7 @@ def input_item_into_search_string(context):
     search.send_keys( 'stainless work table' )
 
 
-@then("Click search buton")
+@then("Click search button")
 def click_search_button(context):
     context.driver.find_element( By.CSS_SELECTOR, "button.btn.btn-info.banner-search-btn" ).click()
 
@@ -34,7 +34,7 @@ def chosen_items_are_here(context):
 def add_last_item_to_cart(context):
     context.driver.find_elements( By.CSS_SELECTOR, "input.btn.btn-cart.btn-small" )[-1].click()
 # wait until pop-up desappears
-sleep(10)
+    sleep(10)
 
 
 @then("Click on cart button")
@@ -46,7 +46,7 @@ def click_on_cart_button(context):
 def click_on_cross_symbol_empty_cart(context):
     context.driver.find_element( By.CSS_SELECTOR, "a.deleteCartItemButton.close" ).click()
 # wait until cart is empthy
-sleep(10)
+    sleep(10)
 
 
 @then("Verify cart is empty")
@@ -54,4 +54,4 @@ def verify_cart_is_empty(context):
     assert '0' in context.driver.find_element(By.ID, "cartItemCountSpan").text
     print('Text in the cart button: ', str(context.driver.find_element(By.ID, "cartItemCountSpan").text), '.')
 
-    driver.quit()
+    #context.driver.quit()
--- features/tests/Webstaurantstore_dt_23_Jan_20.feature
+++ features/tests/Webstaurantstore_dt_23_Jan_20.feature
@@ -1,20 +1,12 @@
 # Created by rapid at 1/25/2020
-Feature:
-  1. Go to https://www.webstaurantstore.com/
-  2.	Search for 'stainless work table'.
-  3.	Check the search result ensuring every product item has the word 'Table' its title.
-  4.	Add the last of found items to Cart.
-  5.	Empty Cart.
+Feature: User deletes item from cart
 
   Scenario: User search, add, and delete item from the cart
     Given I am on Homepage
     Then Input item into search string
-    Then Click search buton
+    Then Click search button
     Then Verify all items with Table in the title are here
     Then Add the last of found items to cart
     Then Click on cart button
     Then Click on cross symbol empty cart
     Then Verify cart is empty
-
-
-
