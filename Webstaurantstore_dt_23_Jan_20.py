8a38c8e0e6ada740f98becd5c49c4178de3bd68f
LupusLudensEst <gurovvic@gmail.com>
2020-01-25 01:12:24 -0500

    upgrade dt 25 Jan 2020

--- Webstaurantstore_dt_23_Jan_20.py
+++ Webstaurantstore_dt_23_Jan_20.py
@@ -2,6 +2,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -12,7 +13,7 @@ driver.implicitly_wait(20)
 # SEARCH_BTN = (By.CSS_SELECTOR, "button.btn.btn-info.banner-search-btn")
 # ALL_ITEMS_1 = (By.CSS_SELECTOR, "a.description")
 # ALL_ITEMS_2 = (By.CSS_SELECTOR, "input.btn.btn-cart.btn-small")
-# CART_BTN = (By.CSS_SELECTOR,  "a.menu-btn")
+#CART_BTN = (By.CSS_SELECTOR,  "a.menu-btn")
 # EMPTHY_CART_BTN_1 = (By.XPATH, "//a[@href='/shoppingcart:cart.emptycart?nojs=1']")
 # EMPTHY_CART_BTN_2 = (By.CSS_SELECTOR, "button.btn.btn-primary")
 # EMPTHY_CART_BTN_2 = (By.XPATH, "//div[@class='modal-backdrop fade show']")
@@ -29,16 +30,21 @@ search.send_keys( 'stainless work table' )
 driver.find_element( By.CSS_SELECTOR, "button.btn.btn-info.banner-search-btn" ).click()
 
 # verify all items with Table in the title are here
-print( 'There are 1: ', len( driver.find_elements( By.CSS_SELECTOR, "a.description" ) ), 'items' )
-print( 'There are 2: ', len( driver.find_elements( By.CSS_SELECTOR, "input.btn.btn-cart.btn-small" ) ), 'items' )
+print( 'There are : ', len( driver.find_elements( By.CSS_SELECTOR, "input.btn.btn-cart.btn-small" ) ), 'items' )
 
-# add the last of found items to cart and empty cart
+# add the last of found items to cart
 driver.find_elements( By.CSS_SELECTOR, "input.btn.btn-cart.btn-small" )[-1].click()
-driver.find_elements( By.CSS_SELECTOR, "a.menu-btn" )[1].click()
-driver.find_element( By.XPATH, "//a[@href='/shoppingcart:cart.emptycart?nojs=1']" ).click()
-try:
-    driver.find_element(By.CSS_SELECTOR, "button.btn.btn-primary")[4].click()
-except ElementClickInterceptedException:
-    driver.find_elements(By.XPATH, "//div[@class='modal-backdrop fade show']").click()
 
-driver.quit()
+# click on cart button
+driver.find_elements( By.CSS_SELECTOR, "span.menu-btn-text" )[1].click()
+
+# click on cross symbol empty cart
+driver.find_element( By.CSS_SELECTOR, "a.deleteCartItemButton.close" ).click()
+
+# verify Your cart is empty
+assert 'Your cart is empty.' in driver.find_element(By.CSS_SELECTOR, "div.empty-cart__inner").text
+assert '0' in driver.find_element(By.CSS_SELECTOR, "cartItemCountSpan").text
+print('Text in the cart field: ', driver.find_element(By.CSS_SELECTOR, "div.empty-cart__inner").text, '.')
+print('Text in the cart button: ', driver.find_element(By.ID, "cartItemCountSpan").text, '.')
+
+# driver.quit()
