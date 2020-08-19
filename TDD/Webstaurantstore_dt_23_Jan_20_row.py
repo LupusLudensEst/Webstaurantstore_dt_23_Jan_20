c3272bf2300b82e410a4f54470b3218734c3f390
Viacheslav Gurov <gurov>
2020-08-18 22:43:13 -0400

    dt 18 aug 2020

---
+++ TDD/Webstaurantstore_dt_23_Jan_20_row.py
@@ -0,0 +1,84 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver: WebDriver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(20)
+
+SEARCH_STRING = (By.ID, "searchval")
+SEARCH_BTN = (By.CSS_SELECTOR, "button.btn.btn-info.banner-search-btn")
+ITEMS_DESCRIPTIONS = (By.CSS_SELECTOR, "a.description")
+ITEMS_TO_CHOOSE = (By.CSS_SELECTOR, "input.btn.btn-cart.btn-small")
+PRODUCTS = (By.CSS_SELECTOR, '.ag-item.gtm-product')
+CART_BTN = (By.NAME, "cart")
+EMPTY_CROSS_SIGN = (By.CSS_SELECTOR, "a.deleteCartItemButton.close")
+CART_ITEM = (By.ID, "cartItemCountSpan")
+
+# I am on Homepage
+driver.get( 'https://www.webstaurantstore.com/' )
+
+
+# Input item into search string
+search = driver.find_element( *SEARCH_STRING )
+search.clear()
+search.send_keys( 'stainless work table' )
+
+
+# Click search button
+driver.find_element( *SEARCH_BTN  ).click()
+
+
+# Verify all items with Table in the title are here
+print('There are : ', len(driver.find_elements( *ITEMS_TO_CHOOSE )), 'items.')
+print('There are : ', len(driver.find_elements( *ITEMS_DESCRIPTIONS)), 'descriptions.')
+products = driver.find_elements( *PRODUCTS )
+for product in list(products):
+    title = product.find_element( *ITEMS_DESCRIPTIONS )
+print('Title:', title.text, '.')
+assert 'Table' in title.text
+
+
+# Add the last of found items to cart
+sleep(8)
+target = driver.find_element(By.CSS_SELECTOR, "input.btn.btn-cart.btn-small")
+actions = ActionChains(driver)
+actions.move_to_element(target)
+sleep(2)
+actions.click(target)
+actions.perform()
+# driver.find_elements( *ITEMS_TO_CHOOSE )[-1].click()
+# wait until pop-up desappears
+sleep(8)
+
+
+# Click on cart button
+target = driver.find_element(By.NAME, "cart")
+actions = ActionChains(driver)
+actions.move_to_element(target)
+sleep(2)
+actions.click(target)
+actions.perform()
+# driver.find_element( *CART_BTN ).click()
+sleep(8)
+
+
+# Click on cross symbol empty cart
+target = driver.find_element(By.CSS_SELECTOR, "a.deleteCartItemButton.close")
+actions = ActionChains(driver)
+actions.move_to_element(target)
+sleep(2)
+actions.click(target)
+actions.perform()
+# driver.find_element( *EMPTY_CROSS_SIGN ).click()
+# wait until cart is empthy
+sleep(4)
+
+
+# Verify cart is empty
+print('Text in the cart button: ', str(driver.find_element( *CART_ITEM ).text),'.')
+
+# Exit
+driver.quit()
--- pages/main_page.py
+++ pages/main_page.py
@@ -9,10 +9,10 @@ class MainPage(Page):
     ITEMS_TO_CHOOSE = (By.CSS_SELECTOR, "input.btn.btn-cart.btn-small")
     ITEMS_DESCRIPTIONS = (By.CSS_SELECTOR, "a.description")
     PRODUCTS = (By.CSS_SELECTOR, '.ag-item.gtm-product')
-    CART_BTN = (By.CSS_SELECTOR, "a[href*='viewcart'] span.btn-primary")
+    CART_BTN = (By.CSS_SELECTOR, "span.btn.btn-small.btn-primary")
     EMPTY_CROSS_SIGN = (By.CSS_SELECTOR, "a.deleteCartItemButton.close")
+    CART_EMPTY_BLOCK = (By.CSS_SELECTOR, "div.cartEmpty")
     CART_ITEM = (By.ID, "cartItemCountSpan")
-    CARD_EMPTY_BLOCK = (By.CSS_SELECTOR, "div.cartEmpty")
 
     def search_word(self, text):
         """
@@ -38,20 +38,21 @@ class MainPage(Page):
             assert 'Table' in title.text
         print('Title:', title.text, '.')
         # wait until cart is empthy
-        sleep(10)
+
 
     def add_last_item_to_cart(self):
         """
         Add the last of found items to cart
         """
         self.driver.find_elements(*self.ITEMS_TO_CHOOSE)[-1].click()
-        sleep(10)
+        sleep(2)
 
     def click_on_cart_button(self):
         """
         Click on cart button
         """
         self.click(*self.CART_BTN)
+        sleep(2)
 
     def click_on_cross_symbol_empty_cart(self):
         """
@@ -64,5 +65,6 @@ class MainPage(Page):
         """
         Verify cart is empty
         """
-        self.wait_for_element_to_be_visible(*self.CARD_EMPTY_BLOCK)
+        self.wait_for_element_to_be_visible(*self.CART_EMPTY_BLOCK)
         cart = self.driver.find_element(*self.CART_ITEM)
+        print(cart.text)
